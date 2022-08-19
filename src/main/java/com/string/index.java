2c41c7ac5eddbfe33234d2f4126064ba3e9ca0dd
29SahanaV <sahanagowda90089@gmail.com>
2022-08-19 17:19:57 +0530

    string

---
+++ src/main/java/com/string/index.java
@@ -0,0 +1,13 @@
+package com.string;
+
+public class index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sa="I am sahana from Blore and completed CSE graduation"; // given string
+		int res=sa.indexOf("completed"); // to find the index of this world
+		System.out.println("the index of a given word in a string is : ");
+		System.out.println(res); //printing the index of the world
+	}
+
+}
